5c4326aa517da59d5f9cd80e92c32a5391110d2e
Anila Amanat <w1techy8@gmail.com>
2025-09-07 12:02:48 +0500

    fix error

--- server.ts
+++ server.ts
@@ -1,5 +1,9 @@
 // server.ts
-import { startPayload } from '@payloadcms/next';
+import { SanitizedConfig } from 'payload';
 import config from './src/payload.config';
 
 export default startPayload(config);
+
+function startPayload(config: Promise<SanitizedConfig>) {
+    throw new Error('Function not implemented.');
+}
