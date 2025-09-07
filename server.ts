85a3835ddf8920ce85dc1e7c155f5df409fc5ccc
Anila Amanat <w1techy8@gmail.com>
2025-09-07 12:38:53 +0500

    fix

--- server.ts
+++
@@ -1,9 +0,0 @@
-// server.ts
-import { SanitizedConfig } from 'payload';
-import config from './src/payload.config';
-
-export default startPayload(config);
-
-function startPayload(config: Promise<SanitizedConfig>) {
-    throw new Error('Function not implemented.');
-}
