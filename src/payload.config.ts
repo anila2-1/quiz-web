0ac39854951d9f4133daba90ab200f32e0eb0eec
Anila Amanat <w1techy8@gmail.com>
2025-09-13 10:38:21 +0500

    fix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -57,7 +57,7 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+      token: process.env.BLOB_READ_WRITE_TOKEN_READ_WRITE_TOKEN || '',
     }),
   ],
 })
