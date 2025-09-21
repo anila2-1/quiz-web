cad061403acd9f95eb6bc3c120fb147c19e52db0
Anila Amanat <w1techy8@gmail.com>
2025-09-21 12:33:59 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,21 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'http',
-  //       hostname: 'localhost',
-  //       port: '3000',
-  //       pathname: '/api/media/file/**', // Optional: restrict path
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'quiz-learn-web.vercel.app', // ← Your production domain
+        pathname: '/api/media/file/**', // ← Path to your media files
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/media/file/**', // ← For local dev
+      },
+    ],
+  },
   webpack: (webpackConfig) => {
     // Extension aliasing
     webpackConfig.resolve.extensionAlias = {
